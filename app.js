6d39634301c48903dab68ac94d7587c62eb10103
dcabana2023 <132019045+dcabana2023@users.noreply.github.com>
2023-07-13 17:11:46 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+let persona1 =require("./modulo/persona")
+
+console.log(" Hola soy " +persona1[0] + " disfruto jugar al " + persona1[1] + " y vivo en " + persona1[2])
---
+++ clase1.js
@@ -0,0 +1,60 @@
+// function sumar(num1, num2){
+//     return num1 + num2
+// }
+// // console.log(sumar(1, 2))
+
+// function operaciones (callback){
+//     let resultado = callback(1, 2)
+//     console.log(resultado)
+// }
+
+// operaciones(sumar)
+
+// objetos literales------------------
+
+// let persona = {
+//     nombre:"Lucas",
+//     edad: 25,
+//     pais: "Argentina"
+// }
+// console.log(persona.edad)
+
+// const perro = {
+//     nonbre: "Max",
+//     ladrar: function(){
+//         console.log("Guau guau")
+//     },
+//     comer: function(comida){
+//         console.log("Estoy comiendo " + comida)
+//     }
+// }
+// perro.ladrar();
+// perro.comer("carne");
+// console.log(perro.nonbre);
+// const empresa = {
+//         nombre: "AAA",
+//         direccion: {
+//             calle: "Avenida",
+//             numero: 123,
+//             barrio: "Almagro"
+//         }
+ 
+    // console.log(empresa.direccion.barrio)
+    // console.table(empresa)
+    
+    // const usuarios = [
+    //     {
+    //         nombre: "Lucas",
+    //         edad: 25
+    //     },
+    //     {
+    //         nombre: "Juan",
+    //         edad: 35
+    //     },
+    //     {
+    //         nombre: "Luis",
+    //         edad: 30
+    //     }
+    // ]
+    // console.log(usuarios[1].nombre)
+
